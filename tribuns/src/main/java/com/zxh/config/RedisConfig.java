069276026d57a04928aa4a4080425e2f0047dfb1
sr <2765500807@qq.com>
2019-03-19 19:17:22 +0800

    sr down

--- tribuns/src/main/java/com/zxh/config/RedisConfig.java
+++
@@ -1,5 +0,0 @@
-package com.zxh.config;
-
-public class RedisConfig {
-	
-}
--- tribuns/src/main/java/com/zxh/dao/BlogDao.java
+++
@@ -1,9 +0,0 @@
-package com.zxh.dao;
-
-import org.apache.ibatis.annotations.Mapper;
-
-import com.zxh.entity.Blog;
-@Mapper
-public interface BlogDao {
-	public Integer addBlog(Blog blog);
-}
